d0e604fde7cb156b8778c995d31d130bf3010c2f
Saymon Damásio <saymon.damasio95@gmail.com>
2021-11-05 10:49:12 -0300

    middleware para verfificar existencia de conta

--- src/routes.js
+++ src/routes.js
@@ -3,37 +3,45 @@ const { v4 } = require('uuid')
 
 const routes = Router()
 
-const costumers = []
+const customers = []
+
+function verifyAnExistingAccount(request, response, next) {
+  const { cpf } = request.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' })
+  }
+
+  request.customer = customer
+
+  return next()
+}
 
 routes.post('/accounts', (request, response) => {
   const { name, cpf } = request.body
 
-  const customerAlreadyExists = costumers.some(customer => customer.cpf === cpf)
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
 
   if (customerAlreadyExists) {
     return response.status(400).json({ error: 'Customer already exists' })
   }
 
-  const consumer = {
+  const customer = {
     id: v4(),
     name,
     cpf,
     statement: [],
   }
 
-  costumers.push(consumer)
+  customers.push(customer)
 
   response.status(201).send()
 })
 
-routes.get('/statement', (request, response) => {
-  const { cpf } = request.headers
-
-  const customer = costumers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' })
-  }
+routes.get('/statement', verifyAnExistingAccount, (request, response) => {
+  const customer = request.customer
 
   return response.json(customer.statement)
 })
